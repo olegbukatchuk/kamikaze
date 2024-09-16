136cdd3152a06d08ecc02efd418c2fb09d76b885
Oleg Bukatchuk <oleg@bukatchuk.com>
2024-09-16 11:53:29 +0300

    release 1.0.0

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 set -euo pipefail
-
+REPO="olegbukatchuk/kamikaze"
 INSTALL="install -m 755 -o root kamikaze-download kamikaze && chmod u+s kamikaze"
 
-curl -s "https://github.com/olegbukatchuk/kamikaze" \
+curl -s "https://api.github.com/repos/${REPO}/releases/latest" \
    | grep "browser_download_url" \
    | cut -d '"' -f 4 \
    | xargs -n1 curl -s -L --output kamikaze-download
