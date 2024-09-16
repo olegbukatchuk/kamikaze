b598ab9333ac72d3d491898767c0047e68f3642d
Oleg Bukatchuk <oleg@bukatchuk.com>
2024-09-16 11:10:04 +0300

    Fix url

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 REPO="kamikaze"
 INSTALL="install -m 755 -o root kamikaze-download kamikaze && chmod u+s kamikaze"
 
-curl -s "https://git.bukatch.uk/tools/${REPO}" \
+curl -s "https://github.com/olegbukatchuk/${REPO}" \
    | grep "browser_download_url" \
    | cut -d '"' -f 4 \
    | xargs -n1 curl -s -L --output kamikaze-download
